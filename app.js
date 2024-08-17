29bfa9be251eccea9ccbc280495e0148dc42a65e
Harneet Sahi <134643674+harneetsahi@users.noreply.github.com>
2024-08-17 20:42:27 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const display = document.querySelector(".display");
+
+// operators
+const divideBtn = document.querySelector(".divide-btn");
+const multiplyBtn = document.querySelector(".multiply-btn");
+const minusBtn = document.querySelector(".minus-btn");
+const addBtn = document.querySelector(".add-btn");
+const equalBtn = document.querySelector(".equal-btn");
+
+const digitsBtn = document.querySelectorAll(".digitBtn");
+const operatorBtn = document.querySelectorAll(".operatorBtn");
+
+const clearBtn = document.querySelector(".clear-btn");
